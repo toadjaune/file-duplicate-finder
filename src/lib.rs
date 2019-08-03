affc144b3ae1f8e8c3c52ffa33fe3f3d3c08a715
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2019-08-03 17:50:19 +0200

    Move logic to a library

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+use std::fs;
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::path::PathBuf;
+use std::error::Error;
+
+pub fn run(paths: Vec<&str>) -> Result<(), Box <dyn Error>> {
+
+    let mut filenames = HashMap::new();
+
+    for path in paths {
+        scan_dir(PathBuf::from(path), &mut filenames).unwrap();
+    }
+    let filenames = remove_single_entries(filenames);
+    text_output(&filenames);
+
+    Ok(())
+}
+
+fn scan_dir(path: PathBuf, map: &mut HashMap<OsString, Vec<OsString>>) -> Result<(), Box<dyn Error>> {
+
+    let dir_listing = fs::read_dir(path)?;
+
+    for file in dir_listing {
+        // println!("Name: {}", path.unwrap().path().display());
+
+        let file = file?;
+
+        // TODO : can't we instead generate an enum and match on it ?
+        // TODO : currently, we consider symlinks as normal files (even if symlinked towards a
+        // directory)
+        if file.file_type()?.is_dir() {
+
+            scan_dir(file.path(), map);
+
+        } else {
+            let files = map.entry(
+                    OsString::from(
+                        file.path().file_name().unwrap()
+                    )
+                ).or_insert(Vec::new());
+            // NB : Canonicalize resolves all symlinks in the path
+            files.push(OsString::from(file.path().canonicalize()?));
+        }
+    }
+    Ok(())
+}
+
+fn remove_single_entries(map: HashMap<OsString, Vec<OsString>>) -> HashMap<OsString, Vec<OsString>> {
+    map.into_iter()
+        .filter( |entry|
+            // NB : len() is constant-time
+            entry.1.len() > 1
+        ).collect()
+}
+
+fn text_output(map: &HashMap<OsString, Vec<OsString>>) {
+    for (filename,filepaths) in map {
+
+        // TODO : Find a way to directly handle OsString without conversion
+        // TODO : Also, avoid cloning the string
+        println!("Duplicate files named {} :", filename.clone().into_string().unwrap());
+        for filepath in filepaths {
+            println!("    {}", filepath.clone().into_string().unwrap());
+        }
+    }
+}
+
+
+
