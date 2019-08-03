d133949879af2ab904b701d2f8ee4d519a7cb257
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2019-08-03 18:26:56 +0200

    Add FilenameDuplicates structure

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,62 @@ use std::error::Error;
 
 pub fn run(paths: Vec<&str>) -> Result<(), Box <dyn Error>> {
 
-    let mut filenames = HashMap::new();
+    let mut filenames = FilenameDuplicates::new();
 
     for path in paths {
         scan_dir(PathBuf::from(path), &mut filenames).unwrap();
     }
-    let filenames = remove_single_entries(filenames);
-    text_output(&filenames);
+    let filenames = filenames.without_single_entries();
+    filenames.text_output();
 
     Ok(())
 }
 
-fn scan_dir(path: PathBuf, map: &mut HashMap<OsString, Vec<OsString>>) -> Result<(), Box<dyn Error>> {
+pub struct FilenameDuplicates {
+    map: HashMap<OsString, Vec<OsString>>
+}
+
+impl FilenameDuplicates {
+
+    fn new() -> FilenameDuplicates {
+        FilenameDuplicates {
+            map: HashMap::new()
+        }
+    }
+
+    fn text_output(&self) {
+        for (filename,filepaths) in &self.map {
+
+            // TODO : Find a way to directly handle OsString without conversion
+            // TODO : Also, avoid cloning the string
+            println!("Duplicate files named {} :", filename.clone().into_string().unwrap());
+            for filepath in filepaths {
+                println!("    {}", filepath.clone().into_string().unwrap());
+            }
+        }
+    }
+
+    // fn remove_single_entries(&mut self) {
+    //     self.map = self.map.into_iter()
+    //         .filter( |entry|
+    //             // NB : len() is constant-time
+    //             entry.1.len() > 1
+    //         ).collect();
+    // }
+
+    fn without_single_entries(self) -> FilenameDuplicates {
+        FilenameDuplicates {
+            map: self.map.into_iter()
+                .filter( |entry|
+                    // NB : len() is constant-time
+                    entry.1.len() > 1
+                ).collect()
+        }
+    }
+
+}
+
+fn scan_dir(path: PathBuf, duplicates: &mut FilenameDuplicates) -> Result<(), Box<dyn Error>> {
 
     let dir_listing = fs::read_dir(path)?;
 
@@ -31,10 +75,10 @@ fn scan_dir(path: PathBuf, map: &mut HashMap<OsString, Vec<OsString>>) -> Result
         // directory)
         if file.file_type()?.is_dir() {
 
-            scan_dir(file.path(), map);
+            scan_dir(file.path(), duplicates);
 
         } else {
-            let files = map.entry(
+            let files = duplicates.map.entry(
                     OsString::from(
                         file.path().file_name().unwrap()
                     )
@@ -45,26 +89,4 @@ fn scan_dir(path: PathBuf, map: &mut HashMap<OsString, Vec<OsString>>) -> Result
     }
     Ok(())
 }
-
-fn remove_single_entries(map: HashMap<OsString, Vec<OsString>>) -> HashMap<OsString, Vec<OsString>> {
-    map.into_iter()
-        .filter( |entry|
-            // NB : len() is constant-time
-            entry.1.len() > 1
-        ).collect()
-}
-
-fn text_output(map: &HashMap<OsString, Vec<OsString>>) {
-    for (filename,filepaths) in map {
-
-        // TODO : Find a way to directly handle OsString without conversion
-        // TODO : Also, avoid cloning the string
-        println!("Duplicate files named {} :", filename.clone().into_string().unwrap());
-        for filepath in filepaths {
-            println!("    {}", filepath.clone().into_string().unwrap());
-        }
-    }
-}
-
-
 
